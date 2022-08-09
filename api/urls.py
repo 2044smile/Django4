2f5da0c1121c25a1320a68197c1a9013839cdcf3
2044smile <2044smile@naver.com>
2022-08-09 11:53:29 +0900

    refactor: add router and ModelViewSet

--- api/urls.py
+++ api/urls.py
@@ -1,8 +1,20 @@
 from django.urls import path
+from rest_framework.routers import DefaultRouter
+
 from . import views
+
+# ModelViewSet
+#  mixins.CreateModelMixin,
+#  mixins.RetrieveModelMixin,
+#  mixins.UpdateModelMixin,
+#  mixins.DestroyModelMixin,
+#  mixins.ListModelMixin,
+#  GenericViewSet
+router = DefaultRouter()
+router.register('items', views.ItemViewSet, basename='items')
 
 urlpatterns = [
     path('list/', views.get_data),
-    path('add/', views.add_item)
-]
+    path('add/', views.add_item),
+] + router.urls
 
